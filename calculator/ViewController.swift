49d81c8b4edbc3a9540abc91b5b69ca57315e731
Amanda <amreinhard@icloud.com>
2018-02-08 21:47:55 -0800

    Partial basic calculator.

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var display: UILabel!
+    var lastOperation: CalcOperation = .none
+    var lastValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pressedNumber(_ sender: Any) {
+        if let numberButton = sender as? UIButton {
+            if lastOperation != .none {
+                let number = Int(display.text!)
+                lastValue = number!
+                display.text = "0"
+            }
+            display.text! += numberButton.titleLabel!.text!
+        }
+    }
+    
+    @IBAction func pressedDelete(_ sender: Any) {
+        display.text = String(display.text!.dropLast())
+    }
+    @IBAction func pressedOperation(_ sender: Any) {
+        if let opButton = sender as? UIButton, let op = CalcOperation(rawValue: opButton.titleLabel!.text!) {
+            if op == .equal{
+                switch lastOperation {
+                    case .add:
+                    let number = Int(display.text!)
+                    display.text = String(lastValue + number!)
+                default: break
+                }
+            }
+            lastOperation = op
+        }
+        
+    }
 }
 
+enum CalcOperation: String{
+    case none
+    case add = "+"
+    case subtract = "-"
+    case multiply = "*"
+    case divide = "/"
+    case modulo = "%"
+    case positiveOrNegative = "+/-"
+    case equal = "="
+}
